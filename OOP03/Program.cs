40254eac7ebdefee62c4376abbb48764910dd867
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-16 12:40:52 +0300

    2

--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -17,9 +17,19 @@ namespace OOP03
             // b) Static Binding (Early Binding): The compiler determines which method to call
             // at compile time based on the reference type .
 
-          //  Dynamic Binding(Late Binding): The runtime determines which
-          //      method implementation to call at run time based
-           //     on the actual object instance stored in memory.
+            //  Dynamic Binding(Late Binding): The runtime determines which
+            //      method implementation to call at run time based
+            //     on the actual object instance stored in memory.
+
+            // 2 
+            // a) Applying sealed to a class prevents other classes from inheriting from it.
+            // b) Sealed Class: Prevents any class from inheriting from it.
+            // Sealed Method: Prevents a derived class from further overriding that specific
+            // A method can only be declared sealed if it is also modifying an inherited override method.
+            // c) No. A sealed method cannot be overridden because the explicit purpose of the sealed modifier on
+            // a method is to break the virtual inheritance chain and lock down the implementation so downstream
+            // derived classes cannot modify it.
+            
         }
     }
 }
