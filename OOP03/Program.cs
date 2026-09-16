54cf36bbd87bd48853ccfe68cc226b84c8746efd
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-16 12:34:52 +0300

    1

--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace OOP03
+﻿using System.Timers;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace OOP03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 1 
+            // a) Method Overloading : Allows a class to have multiple methods with the same name
+            //  but different Parameters.
+            //   Method Overriding :Allows a derived class to provide a specific implementation 
+            //   of a method that is already defined in its base class.
+
+            // b) Static Binding (Early Binding): The compiler determines which method to call
+            // at compile time based on the reference type .
+
+          //  Dynamic Binding(Late Binding): The runtime determines which
+          //      method implementation to call at run time based
+           //     on the actual object instance stored in memory.
         }
     }
 }
